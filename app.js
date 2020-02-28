16a702a1cbb93045388040dc7504cfb04c5b8fba
Gearoid Hawkins <gearoidhawkins@gmail.com>
2020-02-28 15:25:17 +0000

    - Created and linked the stylesheet to the .html file - Made the 'public' folder static so any browser can request files from it - Added an image to the login screen - Removed the old test .html and css files
    NOTE - Server is now run using command (node app.js)

--- app.js
+++ app.js
@@ -1,38 +1,69 @@
+//var mysql = require('mysql'); //TODO not needed - keep for example
 var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var path = require('path');
-var http = require('http');
-var favicon = require('serve-favicon');
 
-var fs = require('fs');
 var dt = require('./public/javascripts/myfirstmodule');
 
+// TODO skipped mySQL connection handleRequest
+
+//Express is what we'll use for our web applications, this includes packages useful in web development, such as sessions and handling HTTP requests, to initialize it we can do:
 var app = express();
 
-// Functions *******************************************************************************************
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./public/pages/test.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('Whoops! File not found!');
-        } else {
-            //response.write("The date and time are currently: " + dt.myDateTime());
-            response.write(data);
-            console.log("Home page requested");
-            console.log(request.url);
-        }
-        response.end();
-    });
-};
+//We now need to let Express know we'll be using some of its packages:
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
-// Code *************************************************************************************************
-
-//TODO Need to create the server using app
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 //app.use(favicon(path.join('public', 'Images', 'android-icon-36x36.png')));
-app.use('/static', express.static('public'));
-//app.listen(3000);
+app.use('/public', express.static('public'));
 
-http.createServer(handleRequest).listen(8080);
+//Make sure to change the secret code for the sessions, the sessions package is what we'll use to determine if the user is logged-in, the
+//bodyParser package will extract the form data from our login.html file.
+
+//We now need to display our login.html file to the client:
+//TODO HOME Page
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/public/pages' + '/login.html'));
+});
+
+//We need to now handle the POST request,
+app.post('/auth', function(request, response) {
+
+  var username = request.body.username;
+  var password = request.body.password;
+
+  console.log('username:' + username);
+  console.log('password:' + password);
+
+  if (username && password) {
+    request.session.loggedin = true;
+		request.session.username = username;
+    response.redirect('/home');
+    //response.send('Incorrect Username and/or Password!');
+    response.end();
+  }
+  else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// //TODO HOME Page
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!' + '\n' +
+									"The date and time are currently: " + dt.myDateTime());
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(8080);
