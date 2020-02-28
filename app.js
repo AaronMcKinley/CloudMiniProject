74eb4552b36128614f7d6b1e2b1f156b2741afa3
Gearoid Hawkins <gearoidhawkins@gmail.com>
2020-02-28 12:12:02 +0000

    - Fixed a compile error with 'app' not declared - Added a test module to print the date - Added node modules to use express
    NOTE - HTML still not finding stylesheet. I'll fix that now

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
+var express = require('express');
 var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
 var fs = require('fs');
-app.use(express.static('/public'));
+var dt = require('./public/javascripts/myfirstmodule');
+var app = express();
+
+
+
+// Functions *******************************************************************************************
 let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('./test.html', null, function (error, data) {
+    fs.readFile('./public/pages/test.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
-            respone.write('Whoops! File not found!');
+            response.write('Whoops! File not found!');
         } else {
+            //response.write("The date and time are currently: " + dt.myDateTime());
             response.write(data);
+            console.log("Home page requested");
         }
         response.end();
     });
 };
+
+
+// Code *************************************************************************************************
+app.use(express.static('public'));
+//app.use(favicon(path.join('public', 'Images', 'android-icon-36x36.png'))); //TODO
 http.createServer(handleRequest).listen(8080);
