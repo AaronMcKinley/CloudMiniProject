35496198a5f1284400c3de6ee675cc840cd99a09
GEAROID HAWKINS - Student <G00308088@gmit.ie>
2020-03-04 18:37:25 +0000

    s

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 
 var dt = require('./public/javascripts/myfirstmodule');
 
+var publicPages = '/public/pages';
 // TODO skipped mySQL connection handleRequest
 
 //Express is what we'll use for our web applications, this includes packages useful in web development, such as sessions and handling HTTP requests, to initialize it we can do:
@@ -17,11 +18,10 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-//app.use(express.static(__dirname + '/public'));
-//app.use(favicon(path.join('public', 'Images', 'android-icon-36x36.png')));
 app.use('/public', express.static('public'));
 
 //Make sure to change the secret code for the sessions, the sessions package is what we'll use to determine if the user is logged-in, the
@@ -30,14 +30,13 @@ app.use('/public', express.static('public'));
 //We now need to display our login.html file to the client:
 //TODO HOME Page
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/public/pages' + '/login.html'));
+	response.sendFile(path.join(__dirname + publicPages + '/login.html'));
 });
 
 app.get('/ForumPage', function(request, response) {
-	response.sendFile(path.join(__dirname + '/public/pages' + '/Forum.html'));
+	response.sendFile(path.join(__dirname + publicPages + '/Forum.html'));
 });
 
-//We need to now handle the POST request,
 app.post('/auth', function(request, response) {
 
   var username = request.body.username;
@@ -50,7 +49,6 @@ app.post('/auth', function(request, response) {
     request.session.loggedin = true;
 		request.session.username = username;
     response.redirect('/home');
-    //response.send('Incorrect Username and/or Password!');
     response.end();
   }
   else {
@@ -62,16 +60,16 @@ app.post('/auth', function(request, response) {
 // //TODO HOME Page
 app.get('/home', function(request, response) {
 	if (request.session.loggedin) {
-		response.sendFile(path.join(__dirname + '/public/pages' + '/homePage.html'));
+		response.sendFile(path.join(__dirname + publicPages + '/homePage.html'));
 		//response.send('Welcome back, ' + request.session.username + '!' + '\n' + "The date and time are currently: " + dt.myDateTime());
 	} else { // User not logged in
 		response.redirect('/');
+		response.end();
 	}
-	//response.end();
 });
 
 app.get('/newUser', function(request, response) {
-	response.sendFile(path.join(__dirname + '/public/pages' + '/signUp.html'));
+	response.sendFile(path.join(__dirname + publicPages + '/signUp.html'));
 });
 
 app.post('/storeUser', function(request, response) {
